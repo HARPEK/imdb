69af058542b7f6ddbbef97a260b2c82de597afe7
Kemeya Harper <kemeya@gmail.com>
2015-04-27 21:41:48 -0400

    uploading classwork

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,41 @@
 class ActorsController < ApplicationController
+  def index
+    @actors = Actor.all
+  end
+
   def show
     @actor = Actor.find(params[:id])
   end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def edit
+    @actor = Actor.find(params[:id])
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to @actor
+    else
+      render :new
+    end
+  end
+
+  def update
+    @actor = Actor.find(params[:id])
+    if @actor.update(actor_params)
+      redirect_to @actor
+    else
+      render :edit
+    end
+  end
+
+  private
+    def actor_params
+      params.require(:actor).permit(:name, :year_of_birth, :thumbnail)
+    end
+
 end
